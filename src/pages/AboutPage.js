a1436b1a2d99a60e9a6debfe2116c0315a8315eb
Isaac-Garza <Igarza957@yahoo.com>
2020-07-28 16:38:48 -0700

    About Page Finished

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import Hero from '../components/Hero'
+import Content from '../components/Content'
+
 
 function AboutPage(props) {
 
     return(
-        <p>
-            About Works!
-        </p>
+        <div>
+            <Hero title={props.title} />
+            
+            <Content>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu dictum varius duis at consectetur lorem donec.</p>
+                <p>Porta lorem mollis aliquam ut porttitor leo a diam. Risus pretium quam vulputate dignissim suspendisse in est ante. Tempor nec feugiat nisl pretium fusce id velit ut tortor.</p> 
+                <p>Lacus luctus accumsan tortor posuere. Tincidunt vitae semper quis lectus. Urna nunc id cursus metus aliquam eleifend mi. Nunc sed augue lacus viverra vitae. Ornare arcu odio ut sem nulla pharetra. Diam vulputate ut pharetra sit amet.</p>
+            </Content>
+        </div>
     );
 }
 
