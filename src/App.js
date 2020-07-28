824ebfe69307ef808dd17c814999eb371223d3b4
Isaac-Garza <Igarza957@yahoo.com>
2020-07-27 18:52:32 -0700

    Page Start

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component() {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      title: 'Isaac Garza',
+      headerLinks: [
+        { title: 'Home', path: '/'},
+        { title: 'About', path: '/aobut'},
+        { title: 'Contact', path: '/contact'},
+      ],
+      home: {
+        title: 'Be Relentless',
+        subtitle: 'Projects that make a difference',
+        text:'Checkout my projects below'
+      }
+    }
+  }
+  render() {
+    return (
+      <div>
+        Hello from React
+      </div>
+    
+      ); 
+  }
+  
 }
 
 export default App;
