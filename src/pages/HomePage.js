c3ae13ae2af927bca4735e9bc9e1df296210939f
Isaac-Garza <Igarza957@yahoo.com>
2020-07-28 12:57:40 -0700

    Hero Page Completed

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
+import Hero from '../components/Hero'
+import Carousel from '../components/Carousel';
+
 function HomePage(props) {
 
     return(
-        <p>
-            Home Works!
-        </p>
+        <>
+            <Hero title={props.title} subTitle= {props.subTitle} text={props.text}/>
+            <Carousel />
+        </>
     );
 }
 
