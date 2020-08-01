6c437b9afc860f70d3f4681b5e5c8a5c1506f53c
Isaac-Garza <Igarza957@yahoo.com>
2020-08-01 12:10:47 -0700

    Website No Stars

--- src/components/stars/stars.jsx
+++ src/components/stars/stars.jsx
@@ -1,33 +1,8 @@
-import React from 'react';
+// import React from 'react';
 
-import style from './stars.module.scss';
-import $ from 'jquery';
+// import style from './stars.module.scss';
+// import $ from 'jquery';
 
-const createStars = () => {
-    for (let i = 0; i < 30; i++) {
-        console.log(i);
-        var numRand = Math.floor(Math.random() * 501),
-            divsize = 500,
-            posx = (numRand * ($('.background').width() - divsize)).toFixed(),
-            posy = (numRand * ($('.background').height() - divsize)).toFixed(),
-            randomAD = Math.random() + 's';
-        let newdiv = $("<div class='star  star" + i + "'></div>").css({
-            'left': posx + 'px',
-            'top': posy + 'px',
-            'animation-delay': randomAD,
-        });
-        newdiv.appendTo('.background');
-    
-    }
-}
 
-const Stars = () => {
 
-    return (
-        <div class={style.background}>
-            {createStars}
-        </div>
-    );
-}
-
-export default Stars;
+// export default stars;
